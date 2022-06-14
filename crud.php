735edd480094d7339dd5c126c9d19b60217c570f
Jeki Gates <jekigates2004@gmail.com>
2022-06-14 15:01:39 +0700

    add auth security

--- crud.php
+++ crud.php
@@ -4,3 +4,31 @@ session_start();
 require "koneksi.php";
 
 $cmd = $_GET["cmd"];
+
+if ($cmd == "login") {
+  $data = json_decode(file_get_contents('php://input'), true);
+  $username = $data["username"];
+  $password = $data["password"];
+
+  if ($username === "admin" && $password === "admin123") {
+    $_SESSION["auth"] = true;
+
+    echo json_encode([
+      "result" => true,
+    ]);
+  } else {
+    echo json_encode([
+      "result" => false,
+    ]);
+  }
+ } else if ($cmd === "checkAuth") {
+  if (isset($_SESSION["auth"])) {
+    echo json_encode([
+      "result" => true,
+    ]);
+  } else {
+    echo json_encode([
+      "result" => false,
+    ]);
+  }
+}
