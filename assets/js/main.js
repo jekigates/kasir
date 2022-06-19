43fb41bbf9f969e875aafa60c932f94829a61c91
Jeki Gates <jekigates2004@gmail.com>
2022-06-19 21:11:00 +0700

    change function check auth and init datatable to global js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,29 @@ function displayMenu(namaMenu) {
   permalinkOverlay.classList.toggle("d-none");
   document.getElementById(namaMenu).classList.toggle("d-none");
 }
+
+function initDataTable(namaTable, init = true) {
+  if (init) {
+    $(`#${namaTable}`).DataTable({
+      ordering: false,
+      searching: false,
+      lengthMenu: [5],
+      lengthChange: false,
+    });
+  } else {
+    $(`#${namaTable}`).DataTable().destroy();
+  }
+}
+
+function checkAuth() {
+  fetch("crud.php?cmd=checkAuth")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result !== true) {
+        // Kalau pengguna tidak login dengan benar
+
+        alert("Maaf, Anda harus login terlebih dahulu.");
+        location.href = "index.html";
+      }
+    });
+}
